16364591d015c53c56726c92b8e903d785294664
Mehmet Furkan Utlu <furkanutlu@outlook.com>
2024-01-12 23:03:51 +0300

    bitmek üzere

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <title>Gazi Yemek</title>
-  <link rel="stylesheet" href="AnaSayfaSlider.css">
+  <link rel="stylesheet" href="index.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
