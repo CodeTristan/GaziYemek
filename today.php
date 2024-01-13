93d7eaab62d2302e45e7a514c697a3a8363af21e
CodeTristan <nohacker112@gmail.com>
2024-01-13 21:48:16 +0300

    Update today.php

--- today.php
+++ today.php
@@ -10,6 +10,15 @@ date_default_timezone_set('Europe/Istanbul');
     else
     {
       $today = date("Y-m-d");
+      $dayofweek = date('w', strtotime($today));
+      if($dayofweek == 6)
+      {
+        $today = date('Y-m-d',strtotime("+2 days"));
+      }
+      else if($dayofweek == 0)
+      {
+        $today = date('Y-m-d',strtotime("+1 days"));
+      }
     }
 
     
